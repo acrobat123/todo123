1452c6820cfaf9146d7d4a2a8f15cddf64c0783c
acrobat123 <gopipanuganti521@gmail.com>
2023-09-25 16:51:23 +0000

    solutiona

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const app = express();
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+app.use(express.json());
+let db = null;
+const initializeServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server Running at http://localhost:3001/");
+    });
+  } catch (e) {
+    console.log(`DB error:${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeServer();
+
+module.exports = app;
